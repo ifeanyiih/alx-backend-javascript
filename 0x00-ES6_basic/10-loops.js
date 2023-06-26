df49c3b6cb57a8c14eb6e2bf521328573927a5d0
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-06-26 18:15:57 +0000

    complete task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const narray = [];
+  for (const item of array) {
+    narray.push(appendString + item);
+  }
+
+  return narray;
+}
