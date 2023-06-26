9ec0c29c2f4bc6d8ddb00663653e0695ae4eb436
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-06-26 18:33:08 +0000

    complete task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return (
+    {
+      allEmployees: { ...employeesList },
+      getNumberOfDepartments(obj) {
+        return Object.keys(obj).length;
+      },
+    }
+  );
+}
