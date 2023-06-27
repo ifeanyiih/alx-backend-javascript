060f3ccf7367426cd11e84240f724a81912a0c36
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-06-27 21:07:39 +0000

    complete task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,6 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .then(() => console.log('Got a response from the API'))
+    .catch(() => new Error());
+}
