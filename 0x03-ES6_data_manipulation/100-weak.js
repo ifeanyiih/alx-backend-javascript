9ef5495128d9dbc81a56cd17b32645aee354eced
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-04 16:40:02 +0000

    update

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ const weakMap = new WeakMap();
 function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-    if (weakMap.get(endpoint) > 5) {
+    if (weakMap.get(endpoint) >= 5) {
       throw new Error('Endpoint load is high');
     }
   } else {
