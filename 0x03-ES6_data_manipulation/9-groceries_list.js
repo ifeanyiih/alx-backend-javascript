747d4b9e1b72568ba84a428e8c663454e56346e1
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-04 13:31:32 +0000

    update

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -3,6 +3,7 @@ export default function groceriesList() {
   map.set('Apples', 10);
   map.set('Tomatoes', 10);
   map.set('Pasta', 1);
+  map.set('Rice', 1);
   map.set('Banana', 5);
   return map;
 }
