87f432ef451092453f6f5d844b8b5b877c1b0319
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-04 13:56:11 +0000

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, ststr) {
   const arr = [];
   for (const str of set.values()) {
     if (str.startsWith(ststr) && (ststr !== '')) {
-      arr.push(str.split(ststr)[1]);
+      arr.push(str.slice(ststr.length));
     }
   }
   return arr.join('-');
