1523a34ccf20ee866b80d3947c9e47800ba6ec8f
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-07-05 11:09:03 +0000

    update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -4,9 +4,14 @@ export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
+    this._super = Car;
+  }
+
+  get [Symbol.species]() {
+    return this._super;
   }
 
   cloneCar() {
-    return new Car();
+    return new this[Symbol.species]();
   }
 }
