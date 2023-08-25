cb110e5d682cefc17b4e0033387b6460e9908d1e
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-25 13:15:22 +0000

    Modify: remove stub, remove sandbox, use sinon.spy

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,26 +4,25 @@ const sinon = require('sinon');
 const assert = require('assert');
 
 describe("Use Utils as a spy", function () {
-    const sandbox = sinon.createSandbox();
+    let spy;
 
     beforeEach(function () {
-        sandbox.spy(console);
+        spy = sinon.spy(console, "log");
     });
 
     afterEach(function () {
-        sandbox.restore();
+        spy.restore();
     });
 
     it("Verify console", () => {
         sendPaymentRequestToApi(100, 20);
-        assert.equal(console.log.args, 'The total is: 120');
-        assert.equal(console.log.calledOnce, true);
+        assert.equal(spy.args, 'The total is: 120');
+        assert.equal(spy.calledOnce, true);
     });
     
     it("console.log logs the correct message", () => {
-        sandbox.stub(Utils, "calculateNumber").callsFake(() => 20);
-        sendPaymentRequestToApi(100, 20)
-        assert.equal(console.log.args, 'The total is: 20'); 
-        assert.equal(console.log.calledOnce, true);
+        sendPaymentRequestToApi(10, 10)
+        assert.equal(spy.args, 'The total is: 20'); 
+        assert.equal(spy.calledOnce, true);
     });
 });
