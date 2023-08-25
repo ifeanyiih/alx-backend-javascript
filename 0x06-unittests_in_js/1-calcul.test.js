451d338276ca4760de916ae8c7b8273a2fa8c9e4
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-25 12:21:37 +0000

    Modify: use strictEqual and add more tests for DIVIDE

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,21 +4,40 @@ const calculateNumber = require('./1-calcul.js');
 describe('CalculateNumber', () => {
     describe('SUM', () => {
         it('should return 6', () => {
-            assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+            assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        });
+
+        it('should return 3', () => {
+            assert.strictEqual(calculateNumber('SUM', 1, 2), 3);
+        });
+
+        it('should return 0', () => {
+            assert.strictEqual(calculateNumber('SUM', -1, 1), 0);
         });
     });
 
     
     describe('SUBTRACT', () => {
         it('should return -4', () => {
-            assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+            assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        });
+
+        it('should return -1', () => {
+            assert.strictEqual(calculateNumber('SUBTRACT', 1, 2), -1);
         });
     });
 
     
     describe('DIVIDE', () => {
         it('should return Error', () => {
-            assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+            assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        });
+    });
+
+    
+    describe('DIVIDE', () => {
+        it('should return 2', () => {
+            assert.strictEqual(calculateNumber('DIVIDE', 6, 2.5), 2);
         });
     });
 });
