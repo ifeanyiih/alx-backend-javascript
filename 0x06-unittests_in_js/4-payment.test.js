fa840961351dea0a3921b410ec5379fa0635c41b
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-25 13:06:40 +0000

    Modify: use stub and spy

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -4,24 +4,13 @@ const sinon = require('sinon');
 const assert = require('assert');
 
 describe("Use Utils as a spy", function () {
-    const sandbox = sinon.createSandbox();
-
-    beforeEach(function () {
-        sandbox.spy(console);
-        sandbox.stub(Utils, "calculateNumber").callsFake(() => 10);
-    });
-
-    afterEach(function () {
-        sandbox.restore();
-    });
-
     it("Utils.calculateNumber is called with the right args", () => {
+        const stub = sinon.stub(Utils, "calculateNumber").callsFake(() => 10);
+        const spy = sinon.spy(console, "log")
         sendPaymentRequestToApi(100, 20);
-        assert.equal(Utils.calculateNumber.calledWith('SUM', 100, 20), true);
-    });
-    
-    it("console.log logs the correct message", () => {
-        sendPaymentRequestToApi(100, 20)
-        assert.equal(console.log.args, 'The total is: 10'); 
+        assert(stub.calledWith('SUM', 100, 20));
+        assert(spy.args, 'The total is: 10');
+        Utils.calculateNumber.restore();
+        console.log.restore();
     });
 });
