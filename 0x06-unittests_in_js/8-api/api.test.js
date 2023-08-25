c863b77755fdedd748000159c20ad0a736f53207
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-25 14:42:50 +0000

    Modify
    specify the statusCode for the / route on success
    set the Content-Type Header
    export app

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,17 +2,19 @@ const chai = require('chai');
 const request = require('request');
 
 describe('Index page', () => {
-    it('should return the correct status code', () => {
+    it('should return the correct status code', (done) => {
         request('http://localhost:7865', (err, response, body) => {
             if (err) console.log(err);
             chai.expect(response.statusCode).to.equal(200);
         });
+        done();
     });
 
-    it('should return the correct result', () => {
+    it('should return the correct result', (done) => {
         request('http://localhost:7865', (err, response, body) => {
             if (err) console.log(err);
             chai.expect(body).to.deep.equal('Welcome to the payment system');
         });
+        done();
     });
 });
