ec768a02cce9517f25ab4f6006f02241e90f0e59
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-25 12:12:30 +0000

    Modify: test a negative numbr

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,18 +3,22 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('CaluculateNumber', function () {
     it('should return 4', function () {
-      assert.equal(calculateNumber(1, 3), 4);
-  });
+      assert.strictEqual(calculateNumber(1, 3), 4);
+    });
     
     it('should return 5', function () {
-        assert.equal(calculateNumber(1.2, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     });
 
     it('should return 6', function () {
-        assert.equal(calculateNumber(1.5, 3.7), 6);
+        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
     
     it('should return  4', function () {
-        assert.equal(calculateNumber(1.7, 2), 4);
+        assert.strictEqual(calculateNumber(1.7, 2), 4);
+    });
+
+    it('should return 1', function () {
+        assert.strictEqual(calculateNumber(-2.8, 4.1), 1);
     });
 });
