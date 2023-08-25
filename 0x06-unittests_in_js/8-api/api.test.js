4bc472d3cf7e8066953c525c55766ac52f01dd19
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-25 14:46:16 +0000

    Modify

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -3,7 +3,7 @@ const request = require('request');
 
 describe('Index page', () => {
     it('should return the correct status code', (done) => {
-        request('http://localhost:7865', (err, response, body) => {
+        request('http://localhost:7865/', (err, response, body) => {
             if (err) console.log(err);
             chai.expect(response.statusCode).to.equal(200);
         });
@@ -11,7 +11,7 @@ describe('Index page', () => {
     });
 
     it('should return the correct result', (done) => {
-        request('http://localhost:7865', (err, response, body) => {
+        request('http://localhost:7865/', (err, response, body) => {
             if (err) console.log(err);
             chai.expect(body).to.deep.equal('Welcome to the payment system');
         });
