cffbed7ea927c36de2600b68146e512caafb8339
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-08-25 12:30:24 +0000

    Modify: Adds more test

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -6,6 +6,14 @@ describe('CalculateNumber', () => {
         it('should return 6', () => {
             expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
         });
+
+        it('should return 3', () => {
+            expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+        });
+
+        it('should return 0', () => {
+            expect(calculateNumber('SUM', -1, 1)).to.equal(0);
+        });
     });
 
     
@@ -13,12 +21,23 @@ describe('CalculateNumber', () => {
         it('should return -4', () => {
             expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
         });
+
+        it('should return -1', () => {
+            expect(calculateNumber('SUBTRACT', 1, 2)).to.equal(-1);
+        });
     });
 
     
     describe('DIVIDE', () => {
         it('should return Error', () => {
             expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+        });
+    });
+
+    
+    describe('DIVIDE', () => {
+        it('should return 2', () => {
+            expect(calculateNumber('DIVIDE', 6, 2.5)).to.equal(2);
         });
     });
 });
